7936922dd823cef7e88761cceaac082efe58f7e3
lcmaze <lcmazeweb@gmail.com>
2022-04-19 18:35:27 -0400

    removed messages link in header

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ template.innerHTML = `
                 <div class="links">
                     <ul>
                         <li><a href="index.html">Home</a></li>
-                        <li><a href="#">Messages</a></li>
+                        
                         <li class="dropdown"><a href="#">My Account</a>
                         <ul class="account-sublinks">
                             <li><a href="profile.html">My Profile</a></li>
